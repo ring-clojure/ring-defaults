778f46ca66aa8cd4c1d243520ea66263625cbddc
James Reeves <jreeves@weavejester.com>
2014-06-07 18:06:43 +0100

    Add multipart params middleware

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-headers "0.1.0"]
-                 [ring/ring-anti-forgery "1.0.0"]]
+                 [ring/ring-anti-forgery "1.0.0"]
+                 [javax.servlet/servlet-api "2.5"]]
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
--- src/ring/middleware/defaults.clj
+++ src/ring/middleware/defaults.clj
@@ -5,6 +5,7 @@
         [ring.middleware.session.cookie :only [cookie-store]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
         [ring.middleware.nested-params :only [wrap-nested-params]]
+        [ring.middleware.multipart-params :only [wrap-multipart-params]]
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.cookies :only [wrap-cookies]]
         [ring.middleware.absolute-redirects :only [wrap-absolute-redirects]]))
@@ -12,14 +13,15 @@
 (def default-options
   {:flash   true
    :session {:store (cookie-store), :cookie-attrs {:http-only true}}
-   :keyword-params true
-   :nested-params  true
+   :keyword-params   true
+   :nested-params    true
+   :multipart-params true
    :params  true
    :cookies true
    :absolute-redirects true
-   :frame-options :sameorigin
-   :content-type-options :nosniff
-   :xss-protection {:enable? true :mode :block}})
+   :xss-protection {:enable? true :mode :block}
+   :frame-options  :sameorigin
+   :content-type-options :nosniff})
 
 (defn- wrap [handler middleware options]
   (if (true? options)
@@ -37,8 +39,9 @@
     (-> handler
         (wrap wrap-flash   (:flash opts))
         (wrap wrap-session (:session opts))
-        (wrap wrap-keyword-params (:keyword-params opts))
-        (wrap wrap-nested-params  (:nested-params opts))
+        (wrap wrap-keyword-params   (:keyword-params opts))
+        (wrap wrap-nested-params    (:nested-params opts))
+        (wrap wrap-multipart-params (:multipart-params opts))
         (wrap wrap-params  (:params opts))
         (wrap wrap-cookies (:cookies opts))
         (wrap wrap-absolute-redirects (:absolute-redirects opts))
