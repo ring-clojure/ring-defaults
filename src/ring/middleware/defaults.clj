7820b422fe02bec4bd2ab583d0b33f1e02c8f00f
James Reeves <jreeves@weavejester.com>
2014-06-07 18:19:45 +0100

    Add anti-forgery middleware

--- src/ring/middleware/defaults.clj
+++ src/ring/middleware/defaults.clj
@@ -5,21 +5,23 @@
         [ring.middleware.session.cookie :only [cookie-store]]
         [ring.middleware.keyword-params :only [wrap-keyword-params]]
         [ring.middleware.nested-params :only [wrap-nested-params]]
+        [ring.middleware.anti-forgery :only [wrap-anti-forgery]]
         [ring.middleware.multipart-params :only [wrap-multipart-params]]
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.cookies :only [wrap-cookies]]
         [ring.middleware.absolute-redirects :only [wrap-absolute-redirects]]))
 
 (def default-options
-  {:flash   true
-   :session {:store (cookie-store), :cookie-attrs {:http-only true}}
+  {:flash        true
+   :anti-forgery true
+   :session      {:store (cookie-store), :cookie-attrs {:http-only true}}
    :keyword-params   true
    :nested-params    true
    :multipart-params true
    :params  true
    :cookies true
    :absolute-redirects true
-   :xss-protection {:enable? true :mode :block}
+   :xss-protection {:enable? true, :mode :block}
    :frame-options  :sameorigin
    :content-type-options :nosniff})
 
@@ -37,14 +39,15 @@
   [handler & [{:as options}]]
   (let [opts (merge default-options options)]
     (-> handler
-        (wrap wrap-flash   (:flash opts))
-        (wrap wrap-session (:session opts))
+        (wrap wrap-flash        (:flash opts))
+        (wrap wrap-anti-forgery (:anti-forgery opts))
+        (wrap wrap-session      (:session opts))
         (wrap wrap-keyword-params   (:keyword-params opts))
         (wrap wrap-nested-params    (:nested-params opts))
         (wrap wrap-multipart-params (:multipart-params opts))
         (wrap wrap-params  (:params opts))
         (wrap wrap-cookies (:cookies opts))
         (wrap wrap-absolute-redirects (:absolute-redirects opts))
-        (wrap wrap-xss-protection  (:xss-protection opts))
-        (wrap x/wrap-frame-options (:frame-options opts))
+        (wrap wrap-xss-protection     (:xss-protection opts))
+        (wrap x/wrap-frame-options    (:frame-options opts))
         (wrap x/wrap-content-type-options (:content-type-options opts)))))
--- test/ring/middleware/defaults_test.clj
+++ test/ring/middleware/defaults_test.clj
@@ -9,26 +9,30 @@
     (let [handler (-> (constantly (response "foo"))
                       (wrap-site-defaults))
           resp    (handler (request :get "/"))]
-      (is (= resp {:status  200
-                   :headers {"X-Frame-Options" "SAMEORIGIN"
-                             "X-Content-Type-Options" "nosniff"
-                             "X-XSS-Protection" "1; mode=block"}
-                   :body    "foo"}))))
+      (is (= (:status resp) 200))
+      (is (= (:body resp) "foo"))
+      (is (= (set (keys (:headers resp)))
+             #{"X-Frame-Options"
+               "X-Content-Type-Options"
+               "X-XSS-Protection"
+               "Set-Cookie"}))
+      (is (= (get-in resp [:headers "X-Frame-Options"]) "SAMEORIGIN"))
+      (is (= (get-in resp [:headers "X-Content-Type-Options"]) "nosniff"))
+      (is (= (get-in resp [:headers "X-XSS-Protection"]) "1; mode=block"))
+      (let [set-cookie (first (get-in resp [:headers "Set-Cookie"]))]
+        (is (.startsWith set-cookie "ring-session="))
+        (is (.contains set-cookie "HttpOnly")))))
 
   (testing "middleware options"
     (let [handler (-> (constantly (response "foo"))
                       (wrap-site-defaults {:frame-options :deny}))
           resp    (handler (request :get "/"))]
-      (is (= resp {:status  200
-                   :headers {"X-Frame-Options" "DENY"
-                             "X-Content-Type-Options" "nosniff"
-                             "X-XSS-Protection" "1; mode=block"}
-                   :body    "foo"}))))
+      (is (= (get-in resp [:headers "X-Frame-Options"]) "DENY"))
+      (is (= (get-in resp [:headers "X-Content-Type-Options"]) "nosniff"))))
 
   (testing "disabled middleware"
     (let [handler (-> (constantly (response "foo"))
-                      (wrap-site-defaults {:frame-options false
-                                           :content-type-options false
-                                           :xss-protection false}))
+                      (wrap-site-defaults {:frame-options false}))
           resp    (handler (request :get "/"))]
-      (is (= resp (response "foo"))))))
+      (is (nil? (get-in resp [:headers "X-Frame-Options"])))
+      (is (= (get-in resp [:headers "X-Content-Type-Options"]) "nosniff")))))
