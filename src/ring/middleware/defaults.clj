42626c1010c31e1a955a8142e0db4e083cec68db
James Reeves <jreeves@weavejester.com>
2014-06-07 15:08:34 +0100

    Add configurable options to defaults middleware

--- src/ring/middleware/defaults.clj
+++ src/ring/middleware/defaults.clj
@@ -1,9 +1,24 @@
 (ns ring.middleware.defaults
   (:use [ring.middleware.params :only [wrap-params]]
-        [ring.middleware.cookies :only [wrap-cookies]]))
+        [ring.middleware.cookies :only [wrap-cookies]]
+        [ring.middleware.x-headers :as x]))
+
+(def default-options
+  {:params true
+   :cookies true
+   :frame-options :sameorigin})
+
+(defn- wrap [handler middleware options]
+  (if (true? options)
+    (middleware handler)
+    (if options
+      (middleware handler options)
+      handler)))
 
 (defn wrap-site-defaults
-  [handler]
-  (-> handler
-      wrap-params
-      wrap-cookies))
+  [handler & [{:as options}]]
+  (let [opts (merge default-options options)]
+    (-> handler
+        (wrap wrap-params (:params opts))
+        (wrap wrap-cookies (:cookies opts))
+        (wrap x/wrap-frame-options (:frame-options opts)))))
--- test/ring/middleware/defaults_test.clj
+++ test/ring/middleware/defaults_test.clj
@@ -9,4 +9,20 @@
     (let [handler (-> (constantly (response "foo"))
                       (wrap-site-defaults))
           resp    (handler (request :get "/"))]
+      (is (= resp {:status  200
+                   :headers {"X-Frame-Options" "SAMEORIGIN"}
+                   :body    "foo"}))))
+
+  (testing "middleware options"
+    (let [handler (-> (constantly (response "foo"))
+                      (wrap-site-defaults {:frame-options :deny}))
+          resp    (handler (request :get "/"))]
+      (is (= resp {:status  200
+                   :headers {"X-Frame-Options" "DENY"}
+                   :body    "foo"}))))
+
+  (testing "disabled middleware"
+    (let [handler (-> (constantly (response "foo"))
+                      (wrap-site-defaults {:frame-options false}))
+          resp    (handler (request :get "/"))]
       (is (= resp (response "foo"))))))
