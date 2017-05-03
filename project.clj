d9b7e1a8417f4d8db17ec041704484c5202a61b6
James Reeves <jreeves@weavejester.com>
2017-05-03 20:37:42 +0100

    Update dependencies to use Ring 1.6.0

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.6.0-beta7"]
-                 [ring/ring-ssl "0.3.0-beta1"]
-                 [ring/ring-headers "0.3.0-beta1"]
-                 [ring/ring-anti-forgery "1.1.0-beta1"]
+                 [ring/ring-core "1.6.0"]
+                 [ring/ring-ssl "0.3.0"]
+                 [ring/ring-headers "0.3.0"]
+                 [ring/ring-anti-forgery "1.1.0"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
   :profiles
