ae8f8c02edb000a3dcf555971d44f367c03de6c9
James Reeves <jreeves@weavejester.com>
2017-07-23 11:20:39 +0100

    Update Clojure 1.9 to alpha17

--- project.clj
+++ project.clj
@@ -15,4 +15,4 @@
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha11"]]}})
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha17"]]}})
