e5e9471ef40993acaf223f989329a520e7c15c24
katsuyasu-murata <katsuyasu.murata@studist.jp>
2020-01-10 14:53:09 +0900

    Update Ring to 1.8.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.6.3"]
+                 [ring/ring-core "1.8.0"]
                  [ring/ring-ssl "0.3.0"]
                  [ring/ring-headers "0.3.0"]
                  [ring/ring-anti-forgery "1.3.0"]
