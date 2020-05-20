263a976a2c60ee642b69ec8de90ab493620b7815
Benjamin Goodger <ben@goodger.nl>
2020-05-20 13:27:16 +0200

    Add Leiningen test profile for Clojure 1.10

--- project.clj
+++ project.clj
@@ -9,10 +9,11 @@
                  [ring/ring-headers "0.3.0"]
                  [ring/ring-anti-forgery "1.3.0"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
-  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:dev  {:dependencies [[ring/ring-mock "0.4.0"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}})
+   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}})
