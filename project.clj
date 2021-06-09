8e96aa798a067b44e819a99c293aacaac6b20030
James Reeves <jreeves@weavejester.com>
2021-06-09 17:48:34 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/ring-clojure/ring-defaults"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.8.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-core "1.9.3"]
                  [ring/ring-ssl "0.3.0"]
                  [ring/ring-headers "0.3.0"]
                  [ring/ring-anti-forgery "1.3.0"]
@@ -12,8 +12,6 @@
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:dev  {:dependencies [[ring/ring-mock "0.4.0"]]}
-   :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}})
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}})
