e797b345c6f67c23e612773144b423571047e7df
James Reeves <jreeves@weavejester.com>
2021-06-09 17:49:29 +0100

    Release 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-defaults "0.3.2"
+(defproject ring/ring-defaults "0.3.3"
   :description "Ring middleware that provides sensible defaults"
   :url "https://github.com/ring-clojure/ring-defaults"
   :license {:name "The MIT License"
