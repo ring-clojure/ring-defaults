c0a806c8d76f1b0f9e83e3b7a2a22424bb6ae35d
James Reeves <jreeves@weavejester.com>
2022-09-19 05:58:48 +0100

    Update Ring dependency to 1.9.6

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.9.3"]
+                 [ring/ring-core "1.9.6"]
                  [ring/ring-ssl "0.3.0"]
                  [ring/ring-headers "0.3.0"]
                  [ring/ring-anti-forgery "1.3.0"]
