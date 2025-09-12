eddbde0e6985aa499c1126a49791d6cbb82c0c35
James Reeves <jreeves@weavejester.com>
2025-09-12 21:23:23 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [ring/ring-core "1.13.0"]
+                 [ring/ring-core "1.15.0"]
                  [ring/ring-ssl "0.4.0"]
                  [ring/ring-headers "0.4.0"]
                  [ring/ring-anti-forgery "1.4.0"]
@@ -12,7 +12,7 @@
   :aliases
   {"test-all" ["with-profile" "default:+1.10:+1.11:+1.12" "test"]}
   :profiles
-  {:dev  {:dependencies [[ring/ring-mock "0.4.0"]]}
+  {:dev  {:dependencies [[ring/ring-mock "0.6.2"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
    :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
-   :1.12 {:dependencies [[org.clojure/clojure "1.12.0"]]}})
+   :1.12 {:dependencies [[org.clojure/clojure "1.12.2"]]}})
